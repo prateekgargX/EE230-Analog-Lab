f607e790e18321ae20ccc4df0603744ff86a771e
Prateek Garg <prateekgarg0020@gmail.com>
2022-01-15 15:52:03 +0530

    Figure to be added

--- Homework-1/plotting_script.py
+++ Homework-1/plotting_script.py
@@ -3,14 +3,14 @@ import numpy as np
 import math
 
 #_______________________________________________________________________________________________________________
-filename     = "RC_Int"              #filename : This is name of the file containing the x and y values
-Plot_Title   =  "This is a plot title"
-x_axis_label = "x_axis_label"
-y_axis_label = "y_axis_label"
+filename     = "Readings"              #filename : This is name of the file containing the x and y values
+Plot_Title   =  "RLC Bandpass Filter"
+x_axis_label = "Frequency"
+y_axis_label = "V(dB)"
 number_of_curves_in_plot = 1
-legend_list = ["a"]
+legend_list = ["Amplitude Frequency Response"]
 LOG_OF_X = True                         #LOG_OF_X : x axis will be logarithmic if LOG_OF_X is 'True'
-is_first_order_derivative = True
+is_first_order_derivative = False
 #_______________________________________________________________________________________________________________
 
 x_axis_colum_number = 1                     #this is column no. from which actual data start. It lowest value is 0.
